0327ad8a2281914559058bf3826df72e82070c0b
Chad <calbers@neomantic.com>
2012-04-14 09:14:43 -0400

    commit a working mongrel2 handler and code that both sends and receives a message. It simply response with 'hello'. It does not touch or handle either the headers or the body sent by mongrel in the zmq message

--- example/handler.rkt
+++ example/handler.rkt
@@ -5,7 +5,7 @@
 (require (planet zitterbewegung/uuid-v4:2:0/uuid-v4))
 
 (start-handler
- (string->bytes/utf-8 (symbol->string (make-uuid)))
- "tcp://*:9996"
- (string->bytes/utf-8 (symbol->string (make-uuid)))
- "tcp://localhost:9997")
+ "tcp://127.0.0.1:9997"
+ "tcp://127.0.0.1:9996"
+ (string->bytes/utf-8 (symbol->string (make-uuid))))
+
--- mongrel2.rkt
+++ mongrel2.rkt
@@ -1,40 +1,83 @@
 (module mongrel2 racket/base
  
   (require ffi/unsafe)
+  (require racket/port)
   (require (prefix-in zmq: (planet jaymccarthy/zeromq:2:1/zmq)))
+  (require (prefix-in tnstr: (planet gerard/tnetstrings:1:0)))
 
   (provide start-handler)
+  (provide parse-mongrel2-msg-part)
+  (provide read-mongrel2-header-msg)
 
-  (struct handler-connection
-          (response-uuid
-           request-uuid
-           incoming-socket
-           outgoing-socket))
-
-  (define (start-handler response-uuid response-endpoint request-uuid request-endpoint)
-    (let ([connection (connect-handler response-uuid response-endpoint request-uuid request-endpoint)])
-      (mainloop connection)))
-
-  (define (mainloop handler-connection)
-    (let ([request-msg (zmq:socket-recv! (handler-connection-incoming-socket handler-connection))])
-      (display "Received Message:")
-      (newline)
-      (display request-msg))
-    (mainloop handler-connection))
-  
-  ;; returns a handler-connection
-   (define (connect-handler response-uuid response-endpoint request-uuid request-endpoint)
+  (define (start-handler request-endpoint response-endpoint response-uuid)
     (let* ([context (zmq:context 1)]
-           [in (zmq:socket context 'PULL)]
-           [out (zmq:socket context 'PUB)])
-      (when request-uuid;;this may be optional
-        (zmq:set-socket-option! in 'IDENTITY request-uuid))
-	  (display "Listening for Mongrel2: ")
-      (zmq:socket-connect! in request-endpoint)
-      (zmq:set-socket-option! out 'IDENTITY response-uuid)
-      (zmq:socket-connect! out response-endpoint)
-      (handler-connection response-uuid request-uuid in out)))
+           [request-socket (zmq:socket context 'PULL)])
+      (display "Connecting incoming\n")
+      (zmq:socket-connect! request-socket request-endpoint)
+      (let ([response-socket (zmq:socket context 'PUB)])
+        (display "Connecting outgoing\n")
+        (zmq:socket-connect! response-socket response-endpoint)
+        (zmq:set-socket-option! response-socket 'IDENTITY response-uuid)
+        (display "Listening\n")
+        (let listen ([listening #t])
+          (if (eqv? running #f)
+              (zmq:socket-close! request-socket)
+              (zmq:socket-close! response-socket)
+              (error 'start-handler "aborting")
+              (let ([request-msg-bytes (zmq:socket-recv! request-socket)])
+                (display "Success\n")
+                (display request-msg-bytes)
+                (newline)
+                (send-response response-socket request-msg-bytes)
+                (listen #t)))))))
+
+  ;;TODO Handle kill message
+
+  ;;Not doing anything with the body
+  ;;Not doing anything with the header
+  
+  (define (send-response socket request-msg-bytes)
+    (let ([mongrel2-header-list (read-mongrel2-header-msg
+                                 (open-input-bytes request-msg-bytes))])
+      (display "sending on socket\n")
+      (zmq:socket-send! socket (make-mongrel2-msg mongrel2-header-list))
+      (display "response sent\n")))
+
+  (define (make-mongrel2-msg mongrel2-header-list)
+    (bytes-append
+     (car mongrel2-header-list)
+     #" "
+     (tnstr:value->bytes/tnetstring (car (cdr mongrel2-header-list)))
+     #" HTTP/1.1 200 OK\r\nContent-Type: text/html;charset=utf-8\r\nContent-Length: 5\r\n\r\nHello\n"))
+
+  (define (parse-mongrel2-msg-part port)
+    (let loop ([msg-fragment #""])
+      (let ([peek (peek-bytes 1 0 port)])
+        (if (eof-object? peek)
+            (error 'read-mongrel2-msg "aborting: hit eof")
+            (if (equal? peek #" ")
+                (begin
+                  (read-bytes 1 port);;increment past the next space
+                  msg-fragment)
+                (loop (bytes-append msg-fragment (read-bytes 1 port))))))))
+  
+  (define (read-mongrel2-header-msg port)
+    (let* ([mongrel2-uuid-bytes (parse-mongrel2-msg-part port)]
+           [source-id-bytes (parse-mongrel2-msg-part port)]
+           [request-path-bytes (parse-mongrel2-msg-part port)])
+      (cond
+       [(not (> (bytes-length mongrel2-uuid-bytes) 0)) (error 'read-mongrel2-request "missing mongrel2 server uuid")]
+       [(not (> (bytes-length source-id-bytes) 0)) (error 'read-mongrel2-request "missing source id")]
+       [(not (> (bytes-length request-path-bytes) 0)) (error 'read-mongrel2-request "missing path")]
+       (else
+        (list mongrel2-uuid-bytes
+              source-id-bytes
+              (bytes->string/utf-8 request-path-bytes))))))
+
 )
+
+   
+  
 
 
 
