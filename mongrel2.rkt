22200f120fe206cca2103de55e707d7dcebe6de0
Chad <calbers@neomantic.com>
2012-04-15 10:57:28 -0400

    added license

--- mongrel2.rkt
+++ mongrel2.rkt
@@ -1,3 +1,21 @@
+;;;;;
+;; A Mongrel2 Adapter written in Racket Scheme
+;; Copyright (C) 2012 Chad Albers <calbers@neomantic.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;;;
+
 (module mongrel2 racket/base
  
   (require ffi/unsafe)
