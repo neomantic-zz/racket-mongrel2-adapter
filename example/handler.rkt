ee511118691062eaa6b304170b550f6de7427cfb
Chad <calbers@neomantic.com>
2012-04-15 23:47:15 -0400

    refactor now passing the struct to the handler giving it access to everything and making it solely responsible for creating the mongrel request

--- example/handler.rkt
+++ example/handler.rkt
@@ -3,13 +3,16 @@
 (require "../mongrel2.rkt")
 
 (require (planet zitterbewegung/uuid-v4:2:0/uuid-v4))
+(require (prefix-in tnstr: (planet gerard/tnetstrings:1:0)))
 
 (run-mongrel2-handler
  #:recv-spec "tcp://127.0.0.1:9997"
  #:send-spec "tcp://127.0.0.1:9996"
  #:send-uuid (symbol->string (make-uuid))
- #:handler (lambda (mongrel2-request-bytes)
-             (display mongrel2-request-bytes)
-             (newline)
-            #" HTTP/1.1 200 OK\r\nContent-Type: text/html;charset=utf-8\r\nContent-Length: 5\r\n\r\nHello\n")
+ #:handler (lambda (mongrel2-msg)
+             (bytes-append
+              (mongrel2-msg-sender-uuid mongrel2-msg)
+              #" "
+              (tnstr:value->bytes/tnetstring (mongrel2-msg-source-id mongrel2-msg))
+              #" HTTP/1.1 200 OK\r\nContent-Type: text/html;charset=utf-8\r\nContent-Length: 5\r\n\r\nHello\n"))
  #:verbose #t)
