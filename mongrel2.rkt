933bdb2fcc00b6f8c06726d693cb404d3bd0d27f
Chad <calbers@neomantic.com>
2012-04-16 23:41:55 -0400

    updated example

--- mongrel2.rkt
+++ mongrel2.rkt
@@ -25,15 +25,17 @@
 ;; #lang racket
 ;; (require "mongrel2.rkt")
 ;; (require (planet zitterbewegung/uuid-v4:2:0/uuid-v4))
-;; (run-mongrel2-handler
+;;   
+;;  (run-mongrel2-handler
 ;;  #:recv-spec "tcp://127.0.0.1:9997"
 ;;  #:send-spec "tcp://127.0.0.1:9996"
 ;;  #:send-uuid (symbol->string (make-uuid))
-;;  #:handler (λ (mongrel2-bytes-msg)
-;;             (display mongrel2-msg)
-;;             #" HTTP/1.1 200 OK\r\nContent-Type: text/html;charset=utf-8\r\nContent-Length: 5\r\n\r\nHello\n")
+;;  #:handler (lambda (mongrel2-request)
+;;              (mongrel2-response
+;;               (mongrel2-request-sender-uuid mongrel2-request)
+;;               (list (mongrel2-request-source-id mongrel2-request))
+;;               "HTTP/1.1 200 OK\r\nContent-Type: text/html;charset=utf-8\r\nContent-Length: 5\r\n\r\nHello\n"))
 ;;  #:verbose #t)
-;;
 ;;;;;
 
 (module mongrel2 racket/base
