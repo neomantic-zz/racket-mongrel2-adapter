2c88426f704fc426750f8f47b0fcebb611af72e4
Chad <calbers@neomantic.com>
2012-04-17 21:09:43 -0400

    added time output to handler example to show that the content is dynamically generated by the handler

--- example/handler.rkt
+++ example/handler.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
+(require racket/date)
 (require "../mongrel2.rkt")
 
 (require (planet zitterbewegung/uuid-v4:2:0/uuid-v4))
@@ -9,8 +10,12 @@
  #:send-spec "tcp://127.0.0.1:9996"
  #:send-uuid (symbol->string (make-uuid))
  #:handler (lambda (mongrel2-request)
-             (mongrel2-response
-              (mongrel2-request-sender-uuid mongrel2-request)
-              (list (mongrel2-request-source-id mongrel2-request))
-              "HTTP/1.1 200 OK\r\nContent-Type: text/html;charset=utf-8\r\nContent-Length: 5\r\n\r\nHello\n"))
+             (let ([date-string (date->string (current-date) #t)])
+               (mongrel2-response
+                (mongrel2-request-sender-uuid mongrel2-request)
+                (list (mongrel2-request-source-id mongrel2-request))
+                (string-append
+                 "HTTP/1.1 200 OK\r\nContent-Type: text/html;charset=utf-8\r\n"
+                 "Content-Length: " (number->string (string-length date-string)) "\r\n\r\n"
+                 date-string "\n"))))
  #:verbose #t)
